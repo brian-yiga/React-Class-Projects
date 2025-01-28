8b5c72f86de785c84ef186e07438ddd898690330
brian yiga <brianyiga@gmail.com>
2025-01-28 11:05:57 +0300

    add search functionality and Weather API

--- world_weather/src/App.jsx
+++ world_weather/src/App.jsx
@@ -4,30 +4,9 @@ import WorldWeather from './components/WorldWeather';
 const App = () => {
     const [Weather, setWeather] = useState(null)
 
-    useEffect (() => {
-        setTimeout(()=>{
-            const sampleWeatherData = {
-                city: "Uganda",
-                temperature: "25°C",
-                conditions: "Cloudy"
-            }
-            setWeather(sampleWeatherData)
-        }, 2000)
-    }, [])
-
-
   return (
     <div>
-        {Weather ? 
-        (<WorldWeather 
-        city={Weather.city}
-        temperature={Weather.temperature}
-        conditions={Weather.conditions}/>)
-        : 
-        (
-            <p>Loading weather data...</p>   
-        )
-    }
+     <WorldWeather />
     </div>
   )
 }
--- world_weather/src/components/WorldWeather.jsx
+++ world_weather/src/components/WorldWeather.jsx
@@ -1,15 +1,70 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-const WorldWeather = ({city, temperature, conditions}) => {
+const WorldWeather = () => {
+  const [search, setSearch] = useState('');
+  const [city, setCity] = useState('');
+  const [temperature, setTemperature] = useState('');
+  const [conditions, setConditions] = useState('');
+
+  const weatherApi = {
+    key: 'f340581a0ed986857075b5f66bba2837',
+    base: 'https://api.openweathermap.org/data/2.5/weather'
+  }
+
+  const searchInput = async () => {
+    try {
+    const response = await fetch(`${weatherApi.base}?q=${search}&units=metric&APPID=${weatherApi.key}`);
+    const result = await response.json();
+
+    console.log(result);
+    setCity(result.name);
+    setTemperature(result.main.temp);
+    setConditions(result.weather[0].description);
+  } catch (error){
+    console.log('Error fetching the data', error);
+  }
+  };
+
+  const handleKeyDown = (e) => {
+    if ((e).key === 'Enter') {
+      searchInput()
+    }
+  };
+
+
+
+  // const searchInput = () => {
+  //   fetch(`${weatherApi.base}?q=${search}&units=metric&APPID=${weatherApi.key}`)
+  //     .then(res => res.json())
+  //     .then((result) => {
+  //       console.log(result);
+  //       setCity(result.name);
+  //       setTemperature(result.main.temp); 
+  //       setConditions(result.weather[0].description);
+  //     })
+  //     .catch((error) => {
+  //       console.error('Error fetching weather data:', error);
+  //     });
+  // };
+
   return (
     <div>
-    <h1>Your World Weather</h1>
-    <h2>Check The Weather For Any City Here</h2>
-    <h4>City: {city}</h4>
-    <h4>Temperature is: {temperature}</h4>
-    <h4>Right now the conditions are: {conditions}</h4>
+      <h1 className='title'>Your World Weather</h1>
+      <h2 className='tagline'>Check The Weather For Any City Here</h2>
+      <div className="inputContainer">
+      <input
+        type="text"
+        placeholder='Enter Your City...'
+        onChange={(e) => setSearch(e.target.value)}
+        onKeyDown={handleKeyDown}
+      />
+      <button onClick={searchInput}>Search</button>
+      </div>
+      {city && <h4>City: {city}</h4>}
+      {temperature && <h4>Temperature: {temperature}°C</h4>}
+      {conditions && <h4>Conditions: {conditions}</h4>}
     </div>
-  )
-}
+  );
+};
 
-export default WorldWeather
+export default WorldWeather;
