7fa7e022b1f87990203e44c2ae5f5fe5ffa855de
brian yiga <brianyiga@gmail.com>
2025-01-28 12:02:43 +0300

    Add quotes API

--- random_quote-generator/src/components/QuoteOfTheDay.jsx
+++ random_quote-generator/src/components/QuoteOfTheDay.jsx
@@ -2,38 +2,53 @@ import React, { useState } from 'react'
 
 
 const QuoteOfTheDay = () => {
-    //assign an array of predefined quotes
-    const quotes = [
-        {
-            "quote": "Be yourself; everyone else is already taken.",
-            "author": "Peter Wilde"
-        },
-        {
-            "quote": "To live is the rarest thing in the world. Most people exist, that is all.",
-            "author": "Oscar Wilde"
-        },
-        {
-            "quote": "True friends stab you in the front.",
-            "author": "Lillian Wilde"
-        },
-        {
-            "quote": "Women are made to be Loved, not understood.",
-            "author": "Brian Wilde"
-        },
-        {
-            "quote": "Be the change that you wish to see in the world.",
-            "author": "Mahatma Gandhi"
-        }
-    ]
-    //Get a quote that will be the default value when the page/App is opened
-    const getRandomQuote = () => quotes[Math.floor(Math.random()*quotes.length)]
-    //set useState 
-    const [quoteHeading, setQuoteHeading] = useState(getRandomQuote())
+    // assign an array of predefined quotes
+    // const quotes = [
+    //     {
+    //         "quote": "Be yourself; everyone else is already taken.",
+    //         "author": "Peter Wilde"
+    //     },
+    //     {
+    //         "quote": "To live is the rarest thing in the world. Most people exist, that is all.",
+    //         "author": "Oscar Wilde"
+    //     },
+    //     {
+    //         "quote": "True friends stab you in the front.",
+    //         "author": "Lillian Wilde"
+    //     },
+    //     {
+    //         "quote": "Women are made to be Loved, not understood.",
+    //         "author": "Brian Wilde"
+    //     },
+    //     {
+    //         "quote": "Be the change that you wish to see in the world.",
+    //         "author": "Mahatma Gandhi"
+    //     }
+    // ]
+    // //Get a quote that will be the default value when the page/App is opened
+    // const getRandomQuote = () => quotes[Math.floor(Math.random()*quotes.length)]
+    // //set useState 
+    // const [quoteHeading, setQuoteHeading] = useState(getRandomQuote())
 
-    //get a function that will get the quote
-    const getQuote = () => {
-        const newQuote = getRandomQuote()
-        setQuoteHeading(newQuote)
+    // //get a function that will get the quote
+    // const getQuote = () => {
+    //     const newQuote = getRandomQuote()
+    //     setQuoteHeading(newQuote)
+    // }
+
+    const apiUrl = fetch('https://zenquotes.io/api/quotes/');
+    const [quoteHeading, setQuoteHeading] = useState({});
+
+    const getQuote = async () => {
+        try {
+            const response = await fetch(apiUrl);
+            const result = await response.json();
+            // Select a random quote from the fetched quotes
+            const randomQuote = result.quotes[Math.floor(Math.random() * result.quotes.length)];
+            setQuoteHeading(randomQuote);
+          } catch (error) {
+            console.error('Error fetching the quotes:', error);
+          }
     }
  
 
